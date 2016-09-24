772e2818ac0ce17f6143cc05b2858ca45764debe
jeremiah <jeremiahrichter@gmail.com>
2016-09-24 18:37:17 -0500

    reset to null style

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import styles from './styles/styles.module.styl'
+require('./app.styl');
 
 const App = React.createClass({
     render: () =>
-        <div
-        className='container'>text text text</div>
+        <div>text text text</div>
 });
 
 const rootNode = document.querySelector('#root');
