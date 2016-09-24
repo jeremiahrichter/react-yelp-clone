bf0a4fa5bb1ef9718a34f792c65e412479ba68bc
jeremiah <jeremiahrichter@gmail.com>
2016-09-24 17:31:45 -0500

    add basic webpack and babel configs

---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path'),
+    join = path.join,
+    resolve = path.resolve;
+const getConfig = require('hjs-webpack');
