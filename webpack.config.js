114d3dba36864afdd2f2b28ea657642f73b674bb
jeremiah <jeremiahrichter@gmail.com>
2016-09-24 17:38:30 -0500

    add base deps and constants to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,20 @@ const path = require('path'),
     join = path.join,
     resolve = path.resolve;
 const getConfig = require('hjs-webpack');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+const root = resolve(__dirname);
+const src = join(root, 'src');
+const dest = join(root, 'dist');
+const modules = join(root, 'node_modules');
+
+var config = getConfig({
+    isDev: isDev,
+    in: join(src, 'app.js'),
+    out: join(dest),
+    clearBeforeBuild: true
+});
+
+module.exports = config;
