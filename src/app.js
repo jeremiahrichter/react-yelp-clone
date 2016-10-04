60d7af654bf3b79e22cd5e9abca30e71d900d1db
jeremiah <jeremiahrichter@gmail.com>
2016-10-04 16:55:37 -0500

    add main routes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,16 @@ import './app.styl';
 import App from 'containers/App/App';
 import ReactDOM from 'react-dom';
 import React from 'react';
-import {browserHistory, Router, Route} from 'react-router';
+import {browserHistory} from 'react-router';
+import makeRoutes from './routes';
 
-const Home = React.createClass({
-    render: function() {
-        return (<div>Hello world</div>)
-    }
-});
-
-const routes = (
-    <Router>
-        <Route path='/' component={Home} />
-    </Router>
-);
+const routes = makeRoutes();
 
 const rootNode = document.querySelector('#root');
-ReactDOM.render(<App history={browserHistory} routes={routes}/>, rootNode);
+
+ReactDOM.render(
+    <App
+        history={browserHistory}
+        routes={routes}/>,
+    rootNode
+);
